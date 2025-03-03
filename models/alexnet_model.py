6f5a5e0aa2688dbe5d508792656cae225730aff2
eladlaks <eladlakss@gmail.com>
2025-03-03 23:27:33 +0700

    update for alexnet classifier

--- models/alexnet_model.py
+++ models/alexnet_model.py
@@ -10,5 +10,16 @@ def get_alexnet_model():
     if wandb.config.ALEX_FREEZE_FEATURES:
         for param in model.features.parameters():
             param.requires_grad = False
-    model.classifier[6] = nn.Linear(in_features, wandb.config.NUM_CLASSES)
+    model.classifier[6] = nn.Sequential(
+        nn.Dropout(0.3),
+        nn.Linear(in_features, 4096),  # First FC layer
+        nn.ReLU(),
+        nn.Dropout(0.3),
+        nn.Linear(4096, 512),  # Second FC layer
+        nn.ReLU(),
+        nn.Linear(
+            512, len(wandb.config.NUM_CLASSES)
+        ),  # Final output layer with correct num_labels
+    )
+    # model.classifier[6] = nn.Linear(in_features, wandb.config.NUM_CLASSES)
     return model

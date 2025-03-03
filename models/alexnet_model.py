7ca086856c18b393396b3664905d70c5eb12bebf
eladlaks <eladlakss@gmail.com>
2025-03-03 23:33:12 +0700

    bug fix

--- models/alexnet_model.py
+++ models/alexnet_model.py
@@ -12,13 +12,13 @@ def get_alexnet_model():
             param.requires_grad = False
     model.classifier[6] = nn.Sequential(
         nn.Dropout(0.3),
-        nn.Linear(in_features, 4096),  # First FC layer
+        nn.Linear(num_ftrs, 4096),  # First FC layer
         nn.ReLU(),
         nn.Dropout(0.3),
         nn.Linear(4096, 512),  # Second FC layer
         nn.ReLU(),
         nn.Linear(
-            512, len(wandb.config.NUM_CLASSES)
+            512, wandb.config.NUM_CLASSES
         ),  # Final output layer with correct num_labels
     )
     # model.classifier[6] = nn.Linear(in_features, wandb.config.NUM_CLASSES)

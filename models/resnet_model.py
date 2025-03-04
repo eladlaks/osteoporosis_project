7f8472108d16d558ebd4d7bc6edf2c7f18706bdd
eladlaks <eladlakss@gmail.com>
2025-03-04 17:13:00 +0700

    complex resnet classifier

--- models/resnet_model.py
+++ models/resnet_model.py
@@ -11,13 +11,27 @@ def get_resnet_model():
         parameter.requires_grad = False
 
     num_ftrs = model.fc.in_features
+    # classifier = nn.Sequential(
+    #     OrderedDict(
+    #         [
+    #             ("fc", nn.Linear(num_ftrs, wandb.config.NUM_CLASSES)),
+    #             ("output", nn.LogSoftmax(dim=1)),
+    #         ]
+    #     )
+    # )
+    # model.fc = classifier
+
     classifier = nn.Sequential(
         OrderedDict(
             [
-                ("fc", nn.Linear(num_ftrs, wandb.config.NUM_CLASSES)),
+                ("fc1", nn.Linear(num_ftrs, 512)),
+                ("relu1", nn.ReLU()),
+                ("dropout", nn.Dropout(0.5)),
+                ("fc2", nn.Linear(512, wandb.config.NUM_CLASSES)),
                 ("output", nn.LogSoftmax(dim=1)),
             ]
         )
     )
     model.fc = classifier
+
     return model
